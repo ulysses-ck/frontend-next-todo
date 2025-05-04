bf71c51feba192bdd1021bca6d75081556171913
Ulises <ulisesapaza.280@gmail.com>
2025-05-04 20:14:32 -0300

    feat: list todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
+import {getCollectionResourceTodoGet,  } from "@/client/sdk.gen";
+
+export default async function Home() {
+  const {data} = await getCollectionResourceTodoGet();
+  
   return (
-    <main>Todo App</main>
+    <main>
+      <h1>Todo App</h1>
+      <ul>
+        {
+          data?._embedded?.todos ? data?._embedded?.todos.map(todo => {
+            return <li key={todo.id}>
+              <h3>Title: {todo.title}</h3>
+            </li>
+          }) : <p>There are not todos</p>
+        }
+      </ul>
+    </main>
   );
 }
