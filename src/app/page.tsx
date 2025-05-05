b69f63d8962118633ad1a951ac0e8ce6c13102d4
Ulises <ulisesapaza.280@gmail.com>
2025-05-05 00:25:52 -0300

    feat: import new components into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,37 @@
-import {getCollectionResourceTodoGet,  } from "@/client/sdk.gen";
+import { getCollectionResourceTodoGet } from "@/client/sdk.gen";
+import { CreateTodo } from "@/components/create-todo";
+import { DeleteTodo } from "@/components/delete-todo";
+import { UpdateTodo } from "@/components/update-todo";
 
 export default async function Home() {
-  const {data} = await getCollectionResourceTodoGet();
-  
+  const { data } = await getCollectionResourceTodoGet();
+
   return (
     <main>
       <h1>Todo App</h1>
+
+      <CreateTodo />
+
       <ul>
-        {
-          data?._embedded?.todos ? data?._embedded?.todos.map(todo => {
-            return <li key={todo.id}>
-              <h3>Title: {todo.title}</h3>
-            </li>
-          }) : <p>There are not todos</p>
-        }
+        {data?._embedded?.todos ? (
+          data?._embedded?.todos.map(
+            ({ id, title, description, completed }) => {
+              return (
+                <li key={id}>
+                  <UpdateTodo
+                    id={id!}
+                    title={title!}
+                    description={description!}
+                    completed={completed!}
+                  />
+                  <DeleteTodo id={id as number} />
+                </li>
+              );
+            }
+          )
+        ) : (
+          <p>There are not todos</p>
+        )}
       </ul>
     </main>
   );
